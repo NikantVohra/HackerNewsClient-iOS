adbd2839f3e84740769c281816c2c834982cf639
nvohra <nvohra@paypal.com>
2015-03-27 11:12:44 +0530

    Initial Commit

---
+++ HackerNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackerNews
+//
+//  Created by Vohra, Nikant on 27/03/15.
+//  Copyright (c) 2015 Vohra, Nikant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
